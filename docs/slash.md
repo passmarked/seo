38c523c5d2570507a6221d1b73da232cfb64aa56
Johann du Toit <exxonno@gmail.com>
2016-10-06 00:24:51 +0200

    Added the rule for trailing slashes

---
+++ docs/slash.md
@@ -0,0 +1,12 @@
+URL's on websites can end either with an trailing slash or without. With a trailing slash normally indicates a directory while the absense of a slash indicates a page. 
+
+While Google has noted that the engine is smart enough to see and correct pages that work with both a ending slash and without (see more at [To slash or not to slash](https://webmasters.googleblog.com/2010/04/to-slash-or-not-to-slash.html)). It is adviseable to either disable directory listing or redirect users to the appreciate version of the page without a trailing slash, just for the sake of good house keeping and optimising your site for search engines.
+
+
+# How do I fix this ?
+ 
+Please make sure that your server is set to redirect to the correct page when the page is accessed with na trailing slash, or vice versa depending on desired outcome.
+
+# Resources
+
+* [To slash or not to slash](https://webmasters.googleblog.com/2010/04/to-slash-or-not-to-slash.html)
---
+++ test/slash.js
@@ -0,0 +1,251 @@
+// modules
+const assert        = require('assert');
+const _             = require('underscore');
+const fs            = require('fs');
+const passmarked    = require('passmarked');
+const testFunc      = require('../lib/rules/slash');
+
+// handle the settings
+describe('slash', function() {
+
+  // handle the error output
+  it('Should not return a error if run on the homepage (/)', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/',
+      testingStatusCode:  200,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should not return a error if run on the homepage (/?query=1)', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/?query=1',
+      testingStatusCode:  200,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should not return a error if the trailling alias of /about (/about/) returns something other than 200', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/about',
+      testingStatusCode:  302,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should return a error if the trailling alias of /about (/about/) returns a 200', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/about',
+      testingStatusCode:  200,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(!rule)
+        assert.fail('Was was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should not return a error if the trailling alias of /about/ (/about) returns something other than 200', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/about/',
+      testingStatusCode:  302,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should return a error if the trailling alias of /about/ (/about) returns a 200', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/about/',
+      testingStatusCode:  200,
+      testingRequestUrl:  'http://example.com'
+
+    }, null, '')
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(!rule)
+        assert.fail('Was was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+});
