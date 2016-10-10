44308cd83ea4413c398d2b801b41b3623a5e24dd
Johann du Toit <exxonno@gmail.com>
2016-10-10 22:51:36 +0200

    Update to check the page content, only if a diff is detected we show the error

--- test/slash.js
+++ test/slash.js
@@ -94,11 +94,12 @@ describe('slash', function() {
     // handle the payload
     var payload = passmarked.createPayload({
 
-      url:                'https://example.com/about',
-      testingStatusCode:  302,
-      testingRequestUrl:  'http://example.com'
+      url:                  'https://example.com/about',
+      testingStatusCode:    302,
+      testingRequestUrl:    'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.b.html').toString()
 
-    }, null, '')
+    }, null, fs.readFileSync('./samples/slash.a.html').toString())
 
     // run the rules
     testFunc(payload, function(err) {
@@ -134,11 +135,12 @@ describe('slash', function() {
     // handle the payload
     var payload = passmarked.createPayload({
 
-      url:                'https://example.com/about',
-      testingStatusCode:  200,
-      testingRequestUrl:  'http://example.com'
+      url:                  'https://example.com/about',
+      testingStatusCode:    200,
+      testingRequestUrl:    'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.c.html').toString()
 
-    }, null, '')
+    }, null, fs.readFileSync('./samples/slash.a.html').toString())
 
     // run the rules
     testFunc(payload, function(err) {
@@ -169,6 +171,88 @@ describe('slash', function() {
   });
 
   // handle the error output
+  it('Should not return a error if the trailling alias of /about (/about/) returns a 200 with diff', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                  'https://example.com/about',
+      testingStatusCode:    200,
+      testingRequestUrl:    'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.b.html').toString()
+
+    }, null, fs.readFileSync('./samples/slash.a.html').toString())
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should not return a error if the trailling alias of /about (/about/) returns a 200 with the same content', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                  'https://example.com/about',
+      testingStatusCode:    200,
+      testingRequestUrl:    'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.c.html').toString()
+
+    }, null, fs.readFileSync('./samples/slash.a.html').toString())
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(!rule)
+        assert.fail('Was expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
   it('Should not return a error if the trailling alias of /about/ (/about) returns something other than 200', function(done) {
 
     // handle the payload
@@ -176,9 +260,10 @@ describe('slash', function() {
 
       url:                'https://example.com/about/',
       testingStatusCode:  302,
-      testingRequestUrl:  'http://example.com'
+      testingRequestUrl:  'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.b.html').toString()
 
-    }, null, '')
+    }, null, fs.readFileSync('./samples/slash.a.html').toString())
 
     // run the rules
     testFunc(payload, function(err) {
@@ -209,16 +294,58 @@ describe('slash', function() {
   });
 
   // handle the error output
-  it('Should return a error if the trailling alias of /about/ (/about) returns a 200', function(done) {
+  it('Should not return a error if the trailling alias of /about/ (/about) returns a 200 with diff content', function(done) {
 
     // handle the payload
     var payload = passmarked.createPayload({
 
       url:                'https://example.com/about/',
       testingStatusCode:  200,
-      testingRequestUrl:  'http://example.com'
+      testingRequestUrl:  'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.a.html').toString()
 
-    }, null, '')
+    }, null, fs.readFileSync('./samples/slash.b.html').toString())
+
+    // run the rules
+    testFunc(payload, function(err) {
+
+      // check for a error
+      if(err)
+        assert.fail('Was not expecting a error');
+
+      // get the rules
+      var rules = payload.getRules();
+
+      // check
+      var rule = _.find(rules, function(item) {
+
+        return item.key == 'slash';
+
+      });
+
+      // check if we found it
+      if(rule)
+        assert.fail('Was not expecting a rule to return');
+
+      // done
+      done();
+
+    });
+
+  });
+
+  // handle the error output
+  it('Should return a error if the trailling alias of /about/ (/about) returns a 200 with the same content', function(done) {
+
+    // handle the payload
+    var payload = passmarked.createPayload({
+
+      url:                'https://example.com/about/',
+      testingStatusCode:  200,
+      testingRequestUrl:  'http://example.com',
+      testingRequestBody:   fs.readFileSync('./samples/slash.a.html').toString()
+
+    }, null, fs.readFileSync('./samples/slash.c.html').toString())
 
     // run the rules
     testFunc(payload, function(err) {
@@ -239,7 +366,7 @@ describe('slash', function() {
 
       // check if we found it
       if(!rule)
-        assert.fail('Was was expecting a rule to return');
+        assert.fail('Was expecting a rule to return');
 
       // done
       done();
